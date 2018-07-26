c35a7c0fabb8dbd344ae1ab8026efcf01ad5a089
Sharmaine Requerme <srqrme@gmail.com>
2018-07-26 18:35:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,10 +17,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |attendee|
-    puts attendee
-  end
-  assign_rooms(attendees).each do |attendee|
-    puts attendee
-  end
+  batch_badge_creator(attendees).each {|attendee| puts attendee}
+  assign_rooms(attendees).each {|attendee| puts attendee}
 end
